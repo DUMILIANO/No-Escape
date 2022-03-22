0e53198955134d0a2e376834e9517e19df62fd29
ItsFelipe23 <Fpalacionaranjo2020@elam.co.uk>
2022-03-22 11:52:02 +0000

    PauseMenu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,23 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+namespace scripts
 {
 
-    public static bool GameIsPaused = false;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PauseMenu : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        public static bool GameIsPaused = false;
+        public GameObject PauseMenuUI;
+        public InventoryUI inventoryUi;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
 
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+
+                if(GameIsPaused)
+                {
+                    Resume();
+                }else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            inventoryUi.cursorIsLocked = true;
+            PauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+
+        public void Pause()
+        {
+            inventoryUi.cursorIsLocked = false;
+            PauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
         }
     }
 }
