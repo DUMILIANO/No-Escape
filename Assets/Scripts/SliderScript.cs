9473e32f43ad6b40e365dd08d95855a4099f2832
ItsFelipe23 <Fpalacionaranjo2020@elam.co.uk>
2022-03-29 23:02:17 +0100

    FullyWorkingVolumeSlider

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    public void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
