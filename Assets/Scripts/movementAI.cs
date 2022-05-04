a323d2c42097cee2c90d0fae4a2feb47bcc1b2dc
safuan-siddik <msiddik2020@elam.co.uk>
2022-05-04 23:45:56 +0100

    sfx to AI

--- Assets/Scripts/movementAI.cs
+++ Assets/Scripts/movementAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 namespace Scripts
 {
@@ -10,6 +11,11 @@ namespace Scripts
         public NavMeshAgent enemy;
         public GameObject player;
         public GameObject collider;
+        public ThirdPersonCharacter character;
+
+        public AudioSource audio; 
+        public AudioClip clip;
+        [SerializeField]AudioClip[] audioClip;
 
         float timer = 0.0f;
         public float waitTime = 2.0f;
@@ -18,6 +24,18 @@ namespace Scripts
         void Start()
         {
             enemy = GetComponent<NavMeshAgent>();
+            enemy.updateRotation = false;
+        }
+        private void Step()
+        {
+            AudioClip sfx = GetRandomClip();
+            audio.PlayOneShot(sfx);
+        }
+        
+        private AudioClip GetRandomClip()
+        {
+            int index = Random.Range(0, audioClip.Length - 1);
+            return audioClip[index];
         }
 
         // Update is called once per frame
@@ -33,19 +51,44 @@ namespace Scripts
                 }
                 timer = timer - waitTime;
                 
-            }        
-        }
-        void OnCollisionEnter(Collision collision) {
-            Debug.Log("collision");
-            if(collision.gameObject.tag == "collider")
-            {
-                Debug.Log("hit");
             }
-            else if(collision.gameObject.tag == "Player")
+            if(enemy.remainingDistance > enemy.stoppingDistance)
+            {
+                character.Move(enemy.desiredVelocity, false, false);
+                //audio.PlayOneShot(clip);
+                //audio.Play();
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+                    
+        }
+        void OnCollisionEnter(Collision collision) 
+        {
+            Debug.Log("collision");
+            if(collision.gameObject.tag == "Player")
             {
                 Debug.Log("Dead");
             }
         }
-    }
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(other.tag == "collider")
+            {
+                Debug.Log("Heartbeat");
+                audio.Play();
+            }
+        }
+        private void OnTriggerExit(Collider other) 
+        {
+            if(other.tag == "collider")
+            {
+                Debug.Log("Heartbeat");
+                audio.Pause();
+            }
+        }
+    } 
 }
 
