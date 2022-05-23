4e3d6df340cd7d7d912db184e80feb46e8828af4
ItsFelipe23 <Fpalacionaranjo2020@elam.co.uk>
2022-05-23 17:03:33 +0100

    minor fixes

--- Assets/Scripts/stove.cs
+++ Assets/Scripts/stove.cs
@@ -16,6 +16,7 @@ namespace Scripts
         public Inventory inventory;
         public holding held;
         public ParticleSystem smoke;
+        public GameObject screwdriverMesh;
         
         
         // Start is called before the first frame update
@@ -49,6 +50,7 @@ namespace Scripts
             held.Remove(ice);  
             Destroy(ice);
             screwdriver.gameObject.layer = 0;
+            screwdriverMesh.gameObject.layer = 0;
             ice = null;
             iceCube = null;
             screwdriverCollider.enabled = true;
