9f39f0482318a56d316b5fb578dcd28f43f4f3f5
ItsFelipe23 <Fpalacionaranjo2020@elam.co.uk>
2022-05-26 12:42:16 +0100

    ?

--- Assets/Scripts/closeLivingroom.cs
+++ Assets/Scripts/closeLivingroom.cs
@@ -10,17 +10,18 @@ namespace Scripts
         public doorController door;
         public bookChecker complete;
         public bool doOnce = true;
+
         public void OnTriggerEnter(Collider other)
         {
-            if(other.tag == "Player" && complete.puzzleComplete == false)
+            if(other.tag == "Player" && complete.puzzleComplete == false && doOnce == true )
             {
                 door.PlayAnimation();
                 door.locked = true;
+                doOnce = false;
             }
 
             else if(other.tag == "Player" && door.locked == true && complete.puzzleComplete == true)
             {
-                door.PlayAnimation();
                 door.locked = false;
             }
         }
