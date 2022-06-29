fd0e8d2ffa570f4efa56947d1608778fed7765db
ItsFelipe23 <101564446+ItsFelipe23@users.noreply.github.com>
2022-06-29 17:13:38 +0100

    asdas

--- Assets/Scripts/phoneTxt.cs
+++ Assets/Scripts/phoneTxt.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class phoneTxt : MonoBehaviour
 {
     public GameObject phoneDropTxt;
-    
+    public GameObject fadeAfter;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +17,7 @@ public class phoneTxt : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         phoneDropTxt.SetActive(true);
+        fadeAfter.SetActive(false);
         yield return new WaitForSeconds(3f);
         phoneDropTxt.SetActive(false);
 
